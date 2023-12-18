a4bc15567731dd5bff6d8bc7cc58e0311efa73d1
gustavokessler <37279803+gustavokessler@users.noreply.github.com>
2023-12-18 10:54:06 -0300

    final adjustments

--- src/app/professor/professor.module.ts
+++ src/app/professor/professor.module.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { DecksComponent } from './decks/decks.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { GameLoginComponent } from './game-login/game-login.component';
 import { GameManagementComponent } from './game-management/game-management.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
@@ -16,10 +20,6 @@ import { NgModule } from '@angular/core';
 import { ProfessorComponent } from './professor.component';
 import { ProfessorRoutingModule } from './professor-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { GameLoginComponent } from './game-login/game-login.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
